ec2f79bcf41e69ef6f23084475aec734691a60ad
lucas <lucasucb2020@gmail.com>
2021-12-27 18:32:33 -0300

    Classe veículo criada no pacote model.entities

---
+++ src/model/entities/Veiculo.java
@@ -0,0 +1,19 @@
+package model.entities;
+
+public class Veiculo {
+	private String modelo;
+	
+	public Veiculo() {
+		
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+	
+}
